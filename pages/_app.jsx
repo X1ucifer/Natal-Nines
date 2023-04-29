850a309d690b58c71bdb791c76ed98219b21d046
1uciferx <portforwarding03@gmail.com>
2023-04-30 03:06:42 +0530

    ga updated

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'slick-carousel/slick/slick.css';
 import '../styles/globals.scss';
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+import React, { useEffect, useState } from "react";
+
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
